7be6f922c2a246db50ccfaeac40b1c256e955e5f
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2022-10-05 23:03:42 +0300

    add homework-03

---
+++ lesson_03/lesson_03.js
@@ -0,0 +1,7 @@
+const firstNumber = Number(prompt("Please enter first number."));
+const secondNumber = Number(prompt("Please enter first number."));
+
+alert(`${firstNumber}+${secondNumber}=${firstNumber + secondNumber}\n
+${firstNumber}-${secondNumber}=${firstNumber - secondNumber}\n
+${firstNumber}*${secondNumber}=${firstNumber * secondNumber}\n
+${firstNumber}/${secondNumber}=${firstNumber / secondNumber}`);
