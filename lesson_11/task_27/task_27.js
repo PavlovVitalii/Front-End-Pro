1f5226ad1233c874b10f88d80e1100de1c9613eb
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2022-11-14 21:41:27 +0200

    add lesson_11 tasck_27

---
+++ lesson_11/task_27/task_27.js
@@ -0,0 +1,22 @@
+// На сторінці інпут та кнопка.
+// При натисканні на кнопку - переходимо за посиланням, яке введено у інпут.
+// Також треба реалізувати перевірку введеного значення, чи вказаний протокол http/https.
+// Якщо протокол не вказаний - додаємо https
+
+const form = document.forms.link;
+const inputText = form.elements["adress"];
+const btn = form.elements["button"];
+console.log(inputText);
+console.log(btn);
+btn.addEventListener("click", openLink);
+
+function openLink() {
+  if (
+    inputText.value.indexOf("http") < 0 ||
+    inputText.value.indexOf("https") < 0
+  ) {
+    inputText.value = `https://www.${inputText.value}`;
+  }
+
+  window.open(inputText.value, "_blanck");
+}
